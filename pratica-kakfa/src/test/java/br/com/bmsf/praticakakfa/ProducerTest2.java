4d8345aaa16418b244b13ece68b4273d25ccfd40
brenomsf <breno_marino@hotmail.com>
2022-02-13 23:21:41 -0300

    teste integrado kafka sucesso

--- pratica-kakfa/src/test/java/br/com/bmsf/praticakakfa/ProducerTest2.java
+++ pratica-kakfa/src/test/java/br/com/bmsf/praticakakfa/ProducerTest2.java
@@ -12,16 +12,21 @@ import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
@@ -31,7 +36,9 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import br.com.bmsf.praticakakfa.controller.CidadeModel;
 import br.com.bmsf.praticakakfa.service.Producer;
@@ -41,13 +48,14 @@ import br.com.bmsf.praticakakfa.service.Producer;
 @EmbeddedKafka(partitions = 1, bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 @SpringBootTest
 @SpringJUnitConfig
+@ExtendWith(SpringExtension.class)
 public class ProducerTest2 {
 
 	@Spy
 	private Producer producer;
 
-	@MockBean
-	private KafkaTemplate<String, String> kafkaTemplate;
+//	@MockBean
+//	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@Autowired
 	private EmbeddedKafkaBroker embeddedKafkaBroker;
@@ -58,6 +66,16 @@ public class ProducerTest2 {
 
 	@BeforeAll
 	void setUp() {
+		
+	    Map<String, Object> configProps = new HashMap<>();
+
+	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,embeddedKafkaBroker.getBrokersAsString());
+	    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+	    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		
+	    ReflectionTestUtils.setField(producer, "kafkatempate", new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps)));
+	    
+	    
 		System.setProperty("spring.kafka.bootstrap-servers", embeddedKafkaBroker.getBrokersAsString());
 
 		Map<String, Object> configs = new HashMap<>(
@@ -88,8 +106,8 @@ public class ProducerTest2 {
 
 		producer.enviar(model);
       
-      ConsumerRecord<String, String> received = records.poll(10, TimeUnit.SECONDS);
-      assertNotNull(received);
+	    ConsumerRecord<String, String> received = records.poll(10, TimeUnit.SECONDS);
+	    assertNotNull(received);
       
 
 //		ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, "cidade.criado");
